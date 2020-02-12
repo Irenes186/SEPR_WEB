2f2e2ebd4e8c2981aa8f35b0e8d3dfa0dd9b4096
Ireaurora <is816@york.ac.uk>
2020-02-12 20:28:03 +0000

    some initial website

---
+++ Sepr/build/precache-manifest.2f9a4fd7d6d1cd18c2be8a218eb85ae1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df234078bfcdee98f86c46f5db7b40cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00f9bf603bd6837922e0",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "c0d3060a5fb4e889462b",
+    "url": "/static/css/main.8884606d.chunk.css"
+  },
+  {
+    "revision": "00f9bf603bd6837922e0",
+    "url": "/static/js/2.51ee26ce.chunk.js"
+  },
+  {
+    "revision": "c0d3060a5fb4e889462b",
+    "url": "/static/js/main.3232eb7a.chunk.js"
+  },
+  {
+    "revision": "4d299fc01a70c1ec6ac7",
+    "url": "/static/js/runtime-main.22f94aee.js"
+  }
+]);
